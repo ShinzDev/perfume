bac969bf021a5add2752ba67f9e6b2846709bed8
Shinaayomi Bamgboye~ <shinaayomibamgboye@gmail.com>
2025-10-24 02:31:14 +0100

    about added

--- app/pages/Home.jsx
+++ app/pages/Home.jsx
@@ -1,6 +1,7 @@
 import Navbar from "../components/navbar";
 import Hero from "../components/hero";
 import Footer from "../components/footer";
+import About from "../components/about";
 
 export default function Home() {
     return(
@@ -9,6 +10,7 @@ export default function Home() {
                 <Navbar/>  
                 <div className="p-5" ></div>                
                 <Hero/>
+                <About/>
                 <Footer/>
            </section> 
         </>
